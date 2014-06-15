db16fd75713ba118ac102056bde18df2e6d67ccc
biotrump <support@biotrump.com>
2014-06-15 15:10:38 +0800

    init

---
+++ debug/rppg.bat
@@ -0,0 +1,4 @@
+REM read from a mp4 
+FaceDetect.exe -flbpcascade_frontalface.xml -ehaarcascade_eye.xml -Fface.mp4
+REM read from a camera device
+REM FaceDetect.exe -flbpcascade_frontalface.xml -ehaarcascade_eye.xml -C-1
